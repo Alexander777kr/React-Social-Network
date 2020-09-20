67f9d5d92dcef8af3bcbc127c95d738b67225a9e
Aleksandr Kabanskiy <alexander.777.kr@gmail.com>
2020-09-20 18:23:47 +0300

    basic routing

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,36 @@
 import React from 'react';
 import s from './Navbar.module.css';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <nav className={s.nav}>
       <ul>
-        <li className={s.item}>Profile</li>
-        <li className={`${s.item} ${s.active}`}>Messages</li>
-        <li className={s.item}>News</li>
-        <li className={s.item}>Music</li>
-        <li className={s.item}>Settings</li>
+        <li className={s.item}>
+          <NavLink to="/profile" activeClassName={s.activeLink}>
+            Profile
+          </NavLink>
+        </li>
+        <li className={`${s.item} ${s.active}`}>
+          <NavLink to="/dialogs" activeClassName={s.activeLink}>
+            Messages
+          </NavLink>
+        </li>
+        <li className={s.item}>
+          <NavLink to="/news" activeClassName={s.activeLink}>
+            News
+          </NavLink>
+        </li>
+        <li className={s.item}>
+          <NavLink to="/music" activeClassName={s.activeLink}>
+            Music
+          </NavLink>
+        </li>
+        <li className={s.item}>
+          <NavLink to="/settings" activeClassName={s.activeLink}>
+            Settings
+          </NavLink>
+        </li>
       </ul>
     </nav>
   );
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,7 +4,7 @@ import MyPosts from './MyPosts/MyPosts';
 
 const Profile = () => {
   return (
-    <div className={s.content}>
+    <div>
       <div>
         <img src="main-logo.jpg" width="100%" height="200px" />
       </div>
